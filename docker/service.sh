ff50006ec621938f897ba8d2eaf1bc7594b165a4
David Cardona <djcardonan@eafit.edu.co>
2023-05-28 16:19:07 -0500

    Minor fix to command

--- docker/service.sh
+++ docker/service.sh
@@ -8,9 +8,9 @@ DOCKER_FILE="${ROOT}/docker-compose.yaml"
 export PSQL_DATA_DIR="${ROOT}/data"
 
 if [[ $1 == "up" ]]; then
-    docker compose --file "$DOCKER_FILE" up --detach
+    docker-compose --file "$DOCKER_FILE" up --detach
 fi
 
 if [[ $1 == "down" ]]; then
-    docker compose --file "$DOCKER_FILE" down
+    docker-compose --file "$DOCKER_FILE" down
 fi
